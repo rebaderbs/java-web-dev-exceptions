c407d986bc976c0aea2f6fd482f5d3d21f99d43f
rebaderbs <rderbak@gmail.com>
2022-09-19 14:28:58 -0500

    finished walkthrough

--- src/org/launchcode/Temperature/TemperatureException.java
+++ src/org/launchcode/Temperature/TemperatureException.java
@@ -1,5 +1,10 @@
 package org.launchcode.Temperature;
 
-public class TemperatureException {
+public class TemperatureException extends Exception {
     // Write code here!
+    public TemperatureException(String message) {
+        super(message);
+    }
+
+
 }
